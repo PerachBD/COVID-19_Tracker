b01ecae65fde91789e0677fe1c348b6e01de28cf
Perach Ben David <perach13@gmail.com>
2020-08-18 16:43:04 +0300

    Updates

---
+++ precache-manifest.6f058d152ef1684c18fea79054f72fd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca18db8da4179c0e0f196f01a3e3cd3",
+    "url": "/COVID-19_Tracker/index.html"
+  },
+  {
+    "revision": "a0bcbcdc8a7753d381fc",
+    "url": "/COVID-19_Tracker/static/css/main.26457702.chunk.css"
+  },
+  {
+    "revision": "244441a48a3817cc1942",
+    "url": "/COVID-19_Tracker/static/js/2.2a092bed.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19_Tracker/static/js/2.2a092bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bcbcdc8a7753d381fc",
+    "url": "/COVID-19_Tracker/static/js/main.696c7b30.chunk.js"
+  },
+  {
+    "revision": "d197064f3279a7c7dff9",
+    "url": "/COVID-19_Tracker/static/js/runtime-main.00af7ce1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19_Tracker/static/media/image.d7265326.png"
+  }
+]);
